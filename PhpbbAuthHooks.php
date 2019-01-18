02243751d1fd7bbbeefcfb3632d542bfef8416ba
AJ Quick <ajquick@gmail.com>
2019-01-18 00:49:39 -0700

    Update PhpbbAuthHooks.php
    Add $wgServer for fully qualified urls.

--- PhpbbAuthHooks.php
+++ PhpbbAuthHooks.php
@@ -22,13 +22,14 @@
 class PhpbbAuthHooks {
 
 	public static function onPersonalUrls( array &$personal_urls, Title $title, SkinTemplate $skin ) {
-		global $wgPhpbbAuthAbsolutePath;
+		global $wgPhpbbAuthAbsolutePath, $wgServer;
 
 		if ( array_key_exists( 'login', $personal_urls ) ) {
-			$personal_urls['login']['href'] = $wgPhpbbAuthAbsolutePath . 'ucp.php?mode=login&redirect=' . urlencode( $_SERVER[ 'REQUEST_URI' ] );		}
+			$personal_urls['login']['href'] = $wgPhpbbAuthAbsolutePath . 'ucp.php?mode=login&redirect=' . urlencode( $wgServer . $_SERVER[ 'REQUEST_URI' ] );
+		}
 
 		if ( array_key_exists( 'anonlogin', $personal_urls ) ) {
-			$personal_urls['anonlogin']['href'] = $wgPhpbbAuthAbsolutePath . 'ucp.php?mode=login&redirect=' . urlencode( $_SERVER[ 'REQUEST_URI' ] );
+			$personal_urls['anonlogin']['href'] = $wgPhpbbAuthAbsolutePath . 'ucp.php?mode=login&redirect=' . urlencode( $wgServer . $_SERVER[ 'REQUEST_URI' ] );
 		}
 
 		return true;
